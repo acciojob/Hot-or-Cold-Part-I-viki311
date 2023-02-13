18f23e4a76fe6f9b479376f1a90940ba5c4120ab
viki311 <47682424+viki311@users.noreply.github.com>
2023-02-13 17:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 function func(){
 	
 	const x = Math.floor(Math.random()*40)-20;
+	// const x = 10;
 	document.getElementById("num").innerText = x;
+	if(document.getElementById("guess").value ==x){
+		document.getElementById("respond").innerText = "Hot";
+	}else {
+		document.getElementById("respond").innerText = "Cold";
+	}
 }
 
 
